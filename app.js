8c847b8606a6493e5f5b73b2b9d191698711293c
AdeSembiring8 <fatwasembiring8@gmail.com>
2023-06-15 21:01:07 +0700

    update

--- app.js
+++ app.js
@@ -1,124 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bcrypt = require('bcrypt');
-const app = express();
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+const recordRouter = require('./routes/record')
 
-mongoose.connect('mongodb+srv://fatwasembiring8:Kembaren_8@cluster0.f36seql.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(err => {
-    console.error('Error connecting to MongoDB :', err);
-  });
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(recordRouter)
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+    console.log("Response success")
+    res.send("Response Success!")
+})
 
-const userSchema = new mongoose.Schema({
-  nama: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  tanggal_lahir: {
-    type: Date,
-    required: true
-  }
-});
-
-const User = mongoose.model('User', userSchema);
-
-app.post('/register', (req, res) => {
-  const { nama, email, password, tanggal_lahir } = req.body;
-
-  User.findOne({ email: email })
-    .then(user => {
-      if (user) {
-        return res.status(400).json({ message: 'Email sudah terdaftar!' });
-      }
-
-      const hashedPassword = bcrypt.hashSync(password, 10);
-
-      const newUser = new User({
-        nama,
-        email,
-        password: hashedPassword,
-        tanggal_lahir
-      });
-
-      newUser.save()
-        .then(savedUser => {
-          console.log('User added:', savedUser.email);
-          return res.status(200).json({ message: 'Registrasi berhasil!', user: savedUser });
-        })
-        .catch(err => {
-          console.error('Error saving user:', err);
-          return res.status(500).json({ message: 'Terjadi kesalahan saat menyimpan data pengguna!' });
-        });
-    })
-    .catch(err => {
-      console.error('Error checking user:', err);
-      return res.status(500).json({ message: 'Terjadi kesalahan saat memeriksa data pengguna!' });
-    });
-});
-
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-
-  User.findOne({ email: email })
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        return res.status(200).json({ message: 'Login berhasil!' });
-      }
-
-      return res.status(401).json({ message: 'Email atau password salah!' });
-    })
-    .catch(err => {
-      console.error('Error finding user:', err);
-      return res.status(500).json({ message: 'Terjadi kesalahan saat mencari data pengguna!' });
-    });
-});
-
-app.post('/reset-password', (req, res) => {
-  const { email } = req.body;
-
-  User.findOne({ email: email })
-    .then(user => {
-      if (user) {
-        return res.status(200).json({ message: 'Email untuk reset password telah dikirim!' });
-      }
-
-      return res.status(404).json({ message: 'Email tidak ditemukan!' });
-    })
-    .catch(err => {
-      console.error('Error finding user:', err);
-      return res.status(500).json({ message: 'Terjadi kesalahan saat mencari data pengguna!' });
-    });
-});
-
-app.get('/users', (req, res) => {
-  User.find()
-    .then(users => {
-      return res.status(200).json(users);
-    })
-    .catch(err => {
-      console.error('Error retrieving users:', err);
-      return res.status(500).json({ message: 'Terjadi kesalahan saat mengambil data pengguna!' });
-    });
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+    console.log("Server is up and listening on " + PORT)
+})

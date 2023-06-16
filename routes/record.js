dd87678117042feb2a7ef3773188441db6b45136
AdeSembiring8 <fatwasembiring8@gmail.com>
2023-06-16 10:03:48 +0700

    update

--- routes/record.js
+++ routes/record.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const multer = require("multer");
+const { Storage } = require('@google-cloud/storage');
 
 // TODO: Sesuaikan konfigurasi database
 const connection = mysql.createConnection({
@@ -13,16 +14,17 @@ const connection = mysql.createConnection({
     database: 'sqlcaps01',
     password: 'sqlcaps01'
 })
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname);
-    },
-});
 
-const upload = multer({ storage: storage }).any();
+//Bucket 
+const storage = new Storage({
+    projectId: 'moonlit-balm-389714',
+    keyFilename: 'key2.json'
+});
+const bucketName = 'caps01/uploads/';
+const bucket = storage.bucket(bucketName);
+
+const multerStorage = multer.memoryStorage();
+const upload = multer({ storage: multerStorage }).any();
 
 router.get("/users", (req, res) => {
     const query = "SELECT * FROM users"
@@ -54,7 +56,7 @@ router.post('/login', (req, res) => {
                     const token = jwt.sign({ email: user.email, userId: user.id }, "your-secret-key");
 
                     // Send the token as a response
-                    res.status(200).send({ message: "Login successful", token });
+                    res.status(200).send({ message: "Login successful", token, userId: user.id });
                 } else {
                     res.status(401).send({ message: "Invalid password" });
                 }
@@ -137,36 +139,78 @@ router.get("/skinDisease/:id", (req, res) => {
 router.get("/allskinDisease", (req, res) => {
     const query = "SELECT * FROM skinDisease";
     connection.query(query, (err, result) => {
-      if (err) {
-        res.status(500).send({ message: err.sqlMessage });
-        console.log(err);
-      } else {
-        res.status(200).send({ diseases: result });
-      }
+        if (err) {
+            res.status(500).send({ message: err.sqlMessage });
+            console.log(err);
+        } else {
+            res.status(200).send({ diseases: result });
+        }
     });
-  });
+});
 
 //API untuk mengunggah gambar
+
+  
 router.post("/uploadImage", verifyToken, (req, res, next) => {
     upload(req, res, (err) => {
         if (err) {
             res.status(400).send({ message: "Gagal mengunggah gambar" });
             console.log(err);
         } else {
-            const idUsers = req.user.userId; // Menggunakan id pengguna yang mengunggah gambar
-            const gambar = req.files[0].filename;
+            const idUsers = req.user.userId;
+            const files = req.files;
 
-            const query = "INSERT INTO inputUsers (idUsers, gambar) VALUES (?, ?)";
-            connection.query(query, [idUsers, gambar], (err, result) => {
-                if (err) {
-                    res.status(500).send({ message: err.sqlMessage });
-                } else {
-                    res.status(201).send({ message: "Gambar berhasil diunggah", insertId: result.insertId });
-                }
+            if (!files || files.length === 0) {
+                res.status(400).send({ message: "Tidak ada gambar yang diunggah" });
+                return;
+            }
+
+            const filePromises = files.map((file) => {
+                const fileName = file.filename;
+                const fileBuffer = file.buffer;
+
+                const blob = bucket.file(fileName);
+                const blobStream = blob.createWriteStream({
+                    resumable: false,
+                    public: true
+                });
+
+                return new Promise((resolve, reject) => {
+                    blobStream.on('error', (err) => {
+                        console.log(err);
+                        reject(err);
+                    });
+
+                    blobStream.on('finish', () => {
+                        resolve(fileName);
+                    });
+
+                    blobStream.end(fileBuffer);
+                });
             });
+
+            Promise.all(filePromises)
+                .then((fileNames) => {
+                    // Simpan informasi gambar ke MySQL
+                    const query = "INSERT INTO inputUsers (idUsers, gambar) VALUES (?, ?)";
+                    const insertValues = fileNames.map((fileName) => [idUsers, fileName]);
+
+                    connection.query(query, [insertValues], (err, result) => {
+                        if (err) {
+                            console.log(err);
+                            res.status(500).send({ message: err.sqlMessage });
+                        } else {
+                            res.status(201).send({ message: "Gambar berhasil diunggah", insertId: result.insertId });
+                        }
+                    });
+                })
+                .catch((err) => {
+                    res.status(500).send({ message: "Gagal mengunggah gambar" });
+                });
         }
     });
 });
+
 
 function verifyToken(req, res, next) {
     const token = req.headers.authorization;
@@ -188,31 +232,31 @@ function verifyToken(req, res, next) {
 //History untuk semua penyakit
 router.get("/uploadHistory", verifyToken, (req, res) => {
     const idUsers = req.user.userId;
-  
+
     const query = "SELECT gambar FROM inputUsers WHERE idUsers = ?";
     connection.query(query, [idUsers], (err, result) => {
-      if (err) {
-        res.status(500).send({ message: err.sqlMessage });
-      } else {
-        res.status(200).send({ history: result });
-      }
+        if (err) {
+            res.status(500).send({ message: err.sqlMessage });
+        } else {
+            res.status(200).send({ history: result });
+        }
     });
-  });
+});
 
-  // Fitur Search Mas
-  router.get("/skinDiseases/search", (req, res) => {
+// Fitur Search Mas
+router.get("/skinDiseases/search", (req, res) => {
     const searchTerm = req.query.searchTerm;
     const query = "SELECT * FROM skinDisease WHERE namaPenyakit LIKE ? OR deskripsi LIKE ?";
     const searchValue = "%" + searchTerm + "%";
-  
+
     connection.query(query, [searchValue, searchValue], (err, result) => {
-      if (err) {
-        res.status(500).send({ message: err.sqlMessage });
-      } else {
-        res.status(200).send({ diseases: result });
-      }
+        if (err) {
+            res.status(500).send({ message: err.sqlMessage });
+        } else {
+            res.status(200).send({ diseases: result });
+        }
     });
-  });
-  
+});
+
 
 module.exports = router;
